b866dbebb4d3936046d575cba1477a45c3bc0c6c
Paulo Massingue <paulomassingue31@gmail.com>
2022-09-22 10:47:01 +0100

    Criacao da classe abastrata de produtos eletronicos
    classe que vai conter todos metodos genericos referentes a produtos eletronicos

---
+++ JavaApplication13/src/Ficha2/ProdutoEletronico.java
@@ -0,0 +1,13 @@
+
+package Ficha2;
+public abstract class ProdutoEletronico {
+    private int nrSerie;
+    private String tipo;
+   
+    public boolean ligar(){
+        return true;
+    }      
+    public boolean desligar(){
+        return false;
+    }
+}
