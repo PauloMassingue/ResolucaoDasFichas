ed7b0a28dcf3f4679f42528cfd539c0efa3ffd02
Paulo Massingue <paulomassingue31@gmail.com>
2022-09-23 02:30:40 +0100

    resolucao do exercicio3b

---
+++ JavaApplication13/src/ficha2exercicio3b/Pobre.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ficha2exercicio3b;
+
+/**
+ *
+ * @author Massingue
+ */
+public class Pobre extends Pessoa {
+    public void trabalha(){
+        
+    }
+    
+}
---
+++ JavaApplication13/src/ficha2exercicio3b/Rica.java
@@ -0,0 +1,18 @@
+
+package ficha2exercicio3b;
+
+
+public class Rica extends Pessoa {
+   private double dinheiro;
+
+    public double getDinheiro() {
+        return dinheiro;
+    }
+
+    public void setDinheiro(double dinheiro) {
+        this.dinheiro = dinheiro;
+    }
+   public void fazCompras(){
+       
+   }
+}
